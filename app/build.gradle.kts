b291f68d265e9ab1ce137593cbf8c5f9cb05ffa9
Quoc Hung Tran <130141478+Hugh-Trann@users.noreply.github.com>
2025-03-26 00:45:51 -0700

    Revert "added Login, Registration, Profile, Profile Update, Event Creation"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,4 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation("com.github.bumptech.glide:glide:4.12.0") //for Glide
-    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0") //for Glide
 }
