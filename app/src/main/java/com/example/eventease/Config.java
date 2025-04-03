4ca8d18f3d43f790b663eee5e6f8e75747bc4283
Hugh-Trann <qhung.trn@gmail.com>
2025-04-02 20:12:29 -0700

    feat: added password reset by OTP email

--- app/src/main/java/com/example/eventease/Config.java
+++ app/src/main/java/com/example/eventease/Config.java
@@ -2,6 +2,5 @@ package com.example.eventease;
 
 public class Config {
     public static final String SENDGRID_API_KEY = "";
-//    public static final String SENDGRID_API_KEY = "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";
     public static final String SENDER_EMAIL = "qhung.trn@gmail.com";
 }
